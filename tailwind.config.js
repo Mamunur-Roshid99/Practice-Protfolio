daac58e9c836e5f77b6819b6cd238ec113fa89c2
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-21 00:12:51 +0600

    Protfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: "480px",
@@ -14,7 +15,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        space: ["Space Grotesk"," sans - serif"],
+        space: ["Space Grotesk", " sans - serif"],
       },
     },
   },
